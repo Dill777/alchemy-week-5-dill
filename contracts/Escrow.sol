3d33f1a80b2b8a10907733057b6e66ecabbc4cff
Adhwaith Menon <adhu@Adhwaiths-MacBook-Pro.local>
2023-03-05 13:48:45 +0530

    Added code

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -6,6 +6,18 @@ contract Escrow {
 	address public beneficiary;
 	address public depositor;
 
+	struct Agent {
+		uint timestamp;
+		uint amount;
+	}
+
+	struct User {
+        uint256 id;
+        address user;
+        uint256 amount;
+        Agent payments;
+    }
+
 	bool public isApproved;
 
 	constructor(address _arbiter, address _beneficiary) payable {
@@ -23,5 +35,14 @@ contract Escrow {
  		require(sent, "Failed to send Ether");
 		emit Approved(balance);
 		isApproved = true;
+	}
+
+	mapping (address => mapping(uint => User)) usersList;
+
+
+	event Usercreated(uint256 _id, address user);
+	function createUser(uint _id, uint _amount) external {
+		usersList[msg.sender][id] = User(id : _id, user: msg.sender, amount : _amount, payments: Amount({timestamp: block.timestamp, amount:_amount}));
+		emit Usercreated(_id, msg.sender);
 	}
 }
